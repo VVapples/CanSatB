cad311dbfe25ce9ed971d7c5175eb3e17e8d1d63
Dappl3 <iamsorasky@outlook.com>
2025-08-24 10:40:21 +0900

    TemplateSetup

---
+++ src/getgps.cpp
@@ -0,0 +1,5 @@
+#include "all.h"
+
+int add(int a, int b) {
+  return a + b;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include <Arduino.h>
+#include "all.h"
+
+//pins SETLATER
+#define GPS_RX_PIN 16
+#define GPS_TX_PIN 17
+#define GPS_COMMS_CONFIG SERIAL_8N1
+#define ULTRASONIC_TRIGGER_PIN 18
+#define ULTRASONIC_ECHO_PIN 19
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //Serial w/ pc
+  Serial.begin(115200);
+
+  //scr
+  
 }
 
 void loop() {
