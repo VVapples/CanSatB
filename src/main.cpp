4796be2943061ded2d2ac4dbd4ea4a8928cf0b60
VVapples <mizuno.sora.b1@s.mail.nagoya-u.ac.jp>
2025-08-26 21:21:01 +0900

    BNO055-Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,72 @@
 #include <Arduino.h>
-#include "all.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
 
-//pins SETLATER
-#define GPS_RX_PIN 16
-#define GPS_TX_PIN 17
-#define GPS_COMMS_CONFIG SERIAL_8N1
-#define ULTRASONIC_TRIGGER_PIN 18
-#define ULTRASONIC_ECHO_PIN 19
+// Create an instance of the BNO055 sensor
+// The default I2C address is 0x28, but can also be 0x29
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  //Serial w/ pc
+void setup(void) {
+  // Start the serial communication for debugging
   Serial.begin(115200);
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+  Serial.println("BNO055 Test");
+  Serial.println("");
 
-  //scr
+  // Initialize the BNO055 sensor
+  if (!bno.begin()) {
+    Serial.print("No BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+
+  // Optional: Display some basic information about the sensor
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print("Sensor:       "); Serial.println(sensor.name);
+  Serial.print("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.println("------------------------------------");
+
+  delay(1000);
+
+  // Set the sensor to operate in NDOF (Nine Degrees of Freedom) mode
+  // This mode fuses accelerometer, gyroscope, and magnetometer data
+  bno.setExtCrystalUse(true);
+}
+
+void loop(void) {
+  // Request the orientation data (Euler angles)
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  // Print the Euler angles (orientation)
+  // event.orientation.x is the heading
+  // event.orientation.y is the roll
+  // event.orientation.z is the pitch
+  Serial.print("Heading (X): ");
+  Serial.print(event.orientation.x, 2); // Print with 2 decimal places
+  Serial.print("\t  Roll (Y): ");
+  Serial.print(event.orientation.y, 2);
+  Serial.print("\t  Pitch (Z): ");
+  Serial.println(event.orientation.z, 2);
+
+  // Get and display the calibration status
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
   
-}
+  Serial.print("Calibration: Sys=");
+  Serial.print(system, DEC);
+  Serial.print(" Gyro=");
+  Serial.print(gyro, DEC);
+  Serial.print(" Accel=");
+  Serial.print(accel, DEC);
+  Serial.print(" Mag=");
+  Serial.println(mag, DEC);
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // A small delay before the next reading
+  delay(100);
 }
